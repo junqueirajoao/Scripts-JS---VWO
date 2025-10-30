35b81cd0a994e32a135ae869d954cc798639d337
Joao Junqueira <junqueira.joao@hotmail.com>
2025-10-30 00:56:52 -0300

    Criação de uma função Javascript que deleta várias sections de uma só vez

--- deletarSections.js
+++ deletarSections.js
@@ -5,3 +5,14 @@ function deletarSecao(num){
 }
 
 deletarSecao(1);
+
+//Funcáo para deletar várias sections de uma vez//
+
+function deletarSecoes(numbers){
+    numbers.forEach(num => {
+        const el = document.getElementById('section-' + num);
+        if (el) el.remove();
+    });
+}
+
+deletarSecoes([2, 4, 5]);
